b14739606ab5bf5d95dc8fd79aed021afd9d908f
Your Name <nghilt19411@gmail.com>
2022-08-26 14:43:21 +0700

    implement redis search and create query & search

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,3 +1,8 @@
+import {client} from '$services/redis'
+import {itemsIndexKey} from '$services/keys'
+import {deserialize} from './deserialize'
+
+
 interface QueryOpts {
 	page: number;
 	perPage: number;
@@ -6,4 +11,33 @@ interface QueryOpts {
 	tag: string;
 }
 
-export const itemsByUser = async (userId: string, opts: QueryOpts) => {};
+export const itemsByUser = async (
+	userId: string, 
+	opts: QueryOpts) => 
+	{
+		const query = `@ownerId:${userId}`
+		const sortCriteria = opts.sortBy && 
+							opts.direction &&
+							{
+								BY:opts.sortBy,
+								DIRECTION:opts.direction
+							};
+		const {total,documents} = await client.ft.search(
+			itemsIndexKey(),
+			query,
+			{	
+				ON:'HASH',
+				sortBy:sortCriteria,
+				LIMIT: {
+					from: opts.page * opts.perPage,
+					size: opts.page
+				}
+			} as any
+		);
+		return {
+			totalPages:Math.ceil(total/opts.perPage),
+			items:documents.map(({id,value})=>{
+				return deserialize(id.replace('item#',''),value as any)
+			})
+		}
+	};
